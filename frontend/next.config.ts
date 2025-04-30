e366171801e70e466962e2f39f1a46bdb863a546
void0xf <150953141+void0xf@users.noreply.github.com>
2025-04-30 20:26:59 +0200

    feat: enable React strict mode and configure image domains in Next.js

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Enable React strict mode for development
+  images: {
+    domains: ["image.tmdb.org", "upload.wikimedia.org"], // Add domains for next/image here, e.g. ['images.example.com']
+  },
 };
 
 export default nextConfig;
